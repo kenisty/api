299f571c2e556f795f9b137bb1b39acba95fbcf8
Pola Eskandar <polaeskandar123@gmail.com>
2023-07-15 08:09:41 +0200

    KEN-10: ADD the return type for delete method in repository abstract class.

--- app/Repositories/RepositoryAbstractClass.php
+++ app/Repositories/RepositoryAbstractClass.php
@@ -12,5 +12,5 @@ abstract class RepositoryAbstractClass
     abstract public function find(string $id): mixed;
     abstract public function create(mixed $entry): mixed;
     abstract public function update(string $id, mixed $entry): mixed;
-    abstract public function delete(string $id);
+    abstract public function delete(string $id): void;
 }
