b9e787e4764a1e92ddc15a47c693d21fc088f6e1
Pola Eskandar <polaeskandar123@gmail.com>
2023-12-10 21:52:14 +0100

    KEN: UPDATE switch to symfony structure

--- routes/v1/api.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-use App\Http\Controllers\User\AuthControllerV1;
-use Illuminate\Support\Facades\Route;
-
-Route::group(['prefix' => '/v1/auth', 'controller' => AuthControllerV1::class], static function (): void {
-    Route::post('register', 'register')->name('auth.register');
-    Route::post('login', 'login')->name('auth.register');
-});
